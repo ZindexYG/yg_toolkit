4190a180eb8ec66b9e66030255f6b64feb78bd44
z-index <yg_zinger@163.com>
2025-10-06 14:45:27 +0800

    style: 更新配色

--- src/Page/Calendar/constants.ts
+++ src/Page/Calendar/constants.ts
@@ -1,5 +1,14 @@
 import type { CalendarSource } from './types'
 
+const dopamine = [
+  '#ff6b6b', // 鲜红
+  '#ffb56b', // 橙黄
+  '#ffd56b', // 明黄
+  '#6bceff', // 亮蓝
+  '#6bffb3', // 亮绿
+  '#d56bff', // 亮紫
+]
+
 /**
  * 可用的日历数据源列表
  * - 中国法定节假日: 来自 NateScarlet/holiday-cn
@@ -10,13 +19,13 @@ export const CALENDAR_SOURCES: CalendarSource[] = [
     id: 'chinese_statutory_holidays',
     label: '中国法定节假日',
     url: 'https://raw.githubusercontent.com/NateScarlet/holiday-cn/master/2025.ics',
-    backgroundColor: '#f97316',
+    backgroundColor: dopamine[0],
   },
   {
     id: 'chinese_24_solar_terms',
     label: '中国二十四节气',
     url: 'https://raw.githubusercontent.com/KaitoHH/24-jieqi-ics/master/23_solar_terms_2015-01-01_2050-12-31.ics',
-    backgroundColor: '#4f46e5',
+    backgroundColor: dopamine[1],
   },
 ]
 
