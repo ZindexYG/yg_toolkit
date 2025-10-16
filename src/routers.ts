54e82c58c6229905969d73fb70d90d76f020d078
z-index <yg_zinger@163.com>
2025-10-16 13:30:54 +0800

    feat: 更新路由

--- src/routers.ts
+++ src/routers.ts
@@ -1,10 +1,11 @@
-import { Calendar as IconCalendar, ImageIcon, ImagePlus, Inbox as InboxIcon } from 'lucide-react'
+import { Calendar as IconCalendar, ImageIcon, ImagePlus, Inbox as InboxIcon, Watch } from 'lucide-react'
 import { createBrowserRouter } from "react-router";
 import Layout from './Layout'
 import Calendar from './Page/Calendar'
 import Generator from './Page/Generator'
 import Markimg from './Page/Markimg'
 import Wallhaven from './Page/Wallhaven'
+import Timestamp from './Page/Timestamp'
 // import Home from './Page/Home'
 
 export interface AppRoute {
@@ -21,8 +22,9 @@ export const appRoutes: AppRoute[] = [
   // { title: '编号生成器', path: '/generator', Component: Generator, icon: InboxIcon },
   { title: '编号生成器', path: '/', Component: Generator, icon: InboxIcon },
   { title: '图片水印工具', path: '/Markimg', Component: Markimg, icon: ImagePlus },
-  { title: '日历', path: '/Calendar', Component: Calendar, icon: IconCalendar },
+  { title: '节假日日历', path: '/Calendar', Component: Calendar, icon: IconCalendar },
   { title: 'Wallhaven', path: '/Wallhaven', Component: Wallhaven, icon: ImageIcon },
+  { title: '时间互转', path: '/Timestamp', Component: Timestamp, icon: Watch },
 ]
 
 // 辅助：根据 pathname 查找匹配的 route（优先精确匹配）
